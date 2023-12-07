3285103b9d349d53add9b6e77ebc35a6a8a2cc4f
renanfabritech <renan.goncalves@fabritech.com.br>
2023-12-07 17:28:41 -0300

    #F510_Python_Impressionador

--- ESTUDOS/PYTHON/Sistema_TKinter/aula2.py
+++ ESTUDOS/PYTHON/Sistema_TKinter/aula2.py
@@ -2,14 +2,22 @@ import tkinter as tk
 
 janela = tk.Tk()
 janela.title("Cotação de Moeda")
-mensagem = tk.Label(janela, text="Sistema de busca de cotação de moedas", 
-                    fg='white', bg='black', width=40, height=10)
-mensagem.pack()
 
-mensagem2 = tk.Label(janela, text="Selecione a moeda desejada", 
-                    fg='white', bg='black', width=40, height=10)
-mensagem2.pack()
+janela.rowconfigure(0, weight=1)
+janela.columnconfigure([0, 1], weight=1)
+
+mensagem = tk.Label(janela, text="Sistema de busca de cotação de moedas", fg='white', bg='black', width=40, height=10)
+                    
+mensagem.grid(row=0, column=0, columnspan=2, sticky="EW")
+
+mensagem2 = tk.Label(janela, text="Selecione a moeda desejada", fg='white', bg='black')
+mensagem2.grid(row=1, column=0)
+
+moeda = tk.Entry()
+moeda.grid(row=1, column=1)
+
+
 
 janela.mainloop()
 
-#F508 - Python impressionador
+#F510 - Python impressionador
