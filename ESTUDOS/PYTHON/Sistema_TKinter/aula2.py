2b2ad8e0a85c8ac805f60905ed037ad1089de848
renanfabritech <renan.goncalves@fabritech.com.br>
2023-12-20 06:56:27 -0300

    #F693 - Python Imperssionador

--- ESTUDOS/PYTHON/Sistema_TKinter/aula2.py
+++ ESTUDOS/PYTHON/Sistema_TKinter/aula2.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
 janela = tk.Tk()
 janela.title("Cotação de Moeda")
@@ -13,11 +14,35 @@ mensagem.grid(row=0, column=0, columnspan=2, sticky="EW")
 mensagem2 = tk.Label(janela, text="Selecione a moeda desejada", fg='white', bg='black')
 mensagem2.grid(row=1, column=0)
 
-moeda = tk.Entry()
+#moeda = tk.Entry()
+#moeda.grid(row=1, column=1)
+
+dicionario_cotacoes = {
+    'Dolar': 5.47,
+    'Euro': 6.54,
+    'BitCoin': 20000,
+    }
+
+moedas = list(dicionario_cotacoes.keys())
+
+moeda = ttk.Combobox(janela, values=moedas)
 moeda.grid(row=1, column=1)
 
+def buscar_cotacao():
+    moeda_preenchida = moeda.get()
+    cotacao_moeda = dicionario_cotacoes.get(moeda_preenchida)
+    mensagem_cotacao = tk.Label(text="Cotação não encontrada")
+    mensagem_cotacao.grid(row=3, column=0)
+    if cotacao_moeda:
+            mensagem_cotacao["text"] = f'Cotação do {moeda_preenchida} é de {cotacao_moeda} reais'
+
+    else:
+          pass # Exibir mnsg de cotação não encontrada
+
+botao = tk.Button(text="Buscar cotação", command=buscar_cotacao)
+botao.grid(row=2, column=1)
 
 
 janela.mainloop()
 
-#F510 - Python impressionador
+#F693 - Python impressionador
