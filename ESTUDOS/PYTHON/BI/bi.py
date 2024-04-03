41c9814021fd769dd8d0d6f8637aacac574db8ae
renan.goncalves@fabritech.com.br <renan.goncalves@fabritech.com.br>
2024-04-03 08:20:15 -0300

    up_proj

--- ESTUDOS/PYTHON/BI/bi.py
+++ ESTUDOS/PYTHON/BI/bi.py
@@ -1,4 +1,19 @@
 import pandas as pd
+import os 
 
-vendas_df = pd.read_csv(r'C:\Users\renan.goncalves\Desktop\teste.csv', sep=';')
-print(vendas_df)
+# Importando os arquivos
+caminho_padrao = r'C:\Users\renan.goncalves\Documents\Python\Python e BI'
+
+# Lendo os arquivos CSV com tratamento de codificação
+cotacao_df = pd.read_csv(os.path.join(caminho_padrao, r'teste.csv'), sep=';')
+lucro_df = pd.read_csv(os.path.join(caminho_padrao, r'teste1.csv'), sep=';')
+
+# Limpando apenas as colunas desejadas
+#cotacao_df = cotacao_df[['moeda']]
+#lucro_df = lucro_df[['cliente']]
+
+# Mesclando e renomeando dataframes
+result = pd.merge(cotacao_df, lucro_df, on='cliente')
+
+# Visualizando as primeiras linhas do dataframe resultante
+print(result.head())
